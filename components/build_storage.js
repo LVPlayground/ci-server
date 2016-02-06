6917e0fdaca42060bef2a9710b52e706f45d98df
Russell Krupke <russell@sa-mp.nl>
2016-02-06 04:26:24 +0000

    Handle log file indexing and displaying

---
+++ components/build_storage.js
@@ -0,0 +1,121 @@
+// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+'use strict';
+
+const fs = require('fs'),
+      path = require('path');
+
+// The build storage provides the ability to read, write and index the builds known by this server.
+// All operations will be done on the filesystem, and thus have to be asynchronous.
+class BuildStorage {
+  constructor(path) {
+    this.path_ = path;
+    this.latestBuilds_ = [];
+
+    // There may be a few seconds after server-boot where the latest build information is not
+    // available, bot honestly we can survive that race condition.
+    this.determineLatestBuilds();
+  }
+
+  // Determines the ten most recent builds handled by the continuous integration server and writes
+  // them to the |latestBuilds_| member. This will be determined based on file modification time.
+  determineLatestBuilds() {
+    fs.readdir(this.path_, (error, filenames) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      // Convert the array |filenames| to promises that load the modification time.
+      const timePromii = filenames.map(filename => this.determineFileModificationTime(filename));
+      Promise.all(timePromii).then(files => {
+        // Sort the list of files by their last modification time. We only care about the ten most
+        // recent files that were written to in the directory.
+        files.sort((lhs, rhs) => {
+          if (!lhs) return -1;
+          if (!rhs) return 1;
+
+          return lhs - rhs;
+        });
+
+        // Read the meta data for the first ten files in the |files| array, and store the result in
+        // the |this.latestBuilds_| member for future synchronous access.
+        const readPromii = files.slice(0, 10).map(file => this.readFileMetaData(file[0]));
+        Promise.all(readPromii).then(builds =>
+            this.latestBuilds_ = builds);
+      });
+    });
+  }
+
+  // Returns a promise that will be resolved when the file modification time of |filename| has been
+  // determined. This method accesses the filesystem and thus is asynchronous.
+  determineFileModificationTime(filename) {
+    return new Promise(resolve => {
+      fs.stat(path.join(this.path_, filename), (error, stats) =>
+          error ? resolve(null) : resolve([ filename, stats.mtime ]));
+    });
+  }
+
+  // Returns a promise that will be resolved when the contents of the file have been read, and the
+  // metadata stored in |filename| can be returned as desired.
+  readFileMetaData(filename) {
+    return new Promise(resolve => {
+      fs.readFile(path.join(this.path_, filename), 'utf8', (error, data) => {
+        const build = this.readBuildFromData(error, data);
+        if (build)
+          resolve({ sha: filename, date: build.date, author: build.author, title: build.title, url: build.url });
+        else
+          resolve(null);
+      });
+    });
+  }
+
+  // Safely interprets |data| as a build output file. If |error| is set, it will be assumed that the
+  // value of |data| should not be considered.
+  readBuildFromData(error, data) {
+    if (error)
+      return null;
+
+    // If the |data| cannot be parsed as JSON data, an exception will be written to the console and
+    // NULL will be returned instead, behaving as if the file does not exist.
+    try {
+      return JSON.parse(data);
+
+    } catch (e) { console.error(e); }
+
+    return null;
+  }
+
+  // Returns an array with the 10 most recent builds on the server. This method is synchronous as
+  // it will be initialized once on server load, and updated on modification.
+  getLatestBuilds() {
+    return this.latestBuilds_;
+  }
+
+  // Returns whether |sha| represents a valid git commit hash.
+  verifySha(sha) {
+    return /^[a-zA-Z0-9]{40}$/.test(sha);
+  }
+
+  // Returns a promise that will resolve with the build information about the build identified by
+  // |sha| when this has been loaded from the filesystem. The promise will be resolved with NULL if
+  // the |sha| is invalid, or if the build can not be found on the operating system.
+  getBuild(sha) {
+    return new Promise(resolve => {
+      // (1) Verify that the given |sha| has been appropriately formatted.
+      if (!this.verifySha(sha)) {
+        resolve(null);
+        return;
+      }
+
+      // (2) Asynchronously load the file from disk. If |error| is set, the file either does not
+      // exist or is not writable by this process. Otherwise the |data| is available.
+      fs.readFile(path.join(this.path_, sha), 'utf8', (error, data) =>
+          resolve(this.readBuildFromData(error, data)));
+    });
+  }
+};
+
+module.exports = BuildStorage;
--- index.js
+++ index.js
@@ -4,13 +4,48 @@
 
 'use strict';
 
-const Configuration = require('./components/configuration'),
+const BuildStorage = require('./components/build_storage'),
+      Configuration = require('./components/configuration'),
       Server = require('./components/server');
 
 // Variables that will be available after the configuration is available.
 let server = null;
+let storage = null;
 
 // -------------------------------------------------------------------------------------------------
+
+// Handler for the /build path, which shares a list of most recent builds or the build output of
+// a specific build when the associated SHA has been included in the request's URL.
+function BuildHandler(request, body, response) {
+  if (request.url.length <= 7 /** len(/build/) **/) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<h1>Most recent builds</h1>');
+    response.write('<ul>');
+
+    for (const build of storage.getLatestBuilds()) {
+      response.write(`<li>[${build.date.substr(0, 10)}] <b>${build.author}</b>: `);
+      response.write(`<a href="${build.url}">${build.title}</a> `);
+      response.write(`(<a href="/build/${build.sha}">log</a>)</li>`);
+    }
+
+    response.write('</ul>');
+    response.end();
+    return;
+  }
+
+  // Asynchronously retrieve the build information from the filesystem. |build| being set to NULL
+  // indicates that the build could not be found for some reason.
+  return storage.getBuild(request.url.substr(7)).then(build => {
+    if (!build) {
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.end();
+      return;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(build.log);
+  });
+}
 
 // Handler for /robots.txt to tell spiders that nothing on the server should be indexed.
 function RobotHandler(request, body, response) {
@@ -25,6 +60,9 @@ function RobotHandler(request, body, response) {
 Configuration.load('config.json').then(configuration => {
   server = new Server(configuration['bind_host'], configuration['bind_port']);
 
+  server.registerHandler('/build', BuildHandler);
   server.registerHandler('/robots.txt', RobotHandler);
+
+  storage = new BuildStorage(configuration['storage_path']);
 
 }).catch(error => console.error(error));
